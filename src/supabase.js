47a0a0eba2f9f7716f30fcdc661ef1a7e26f79a1
Chisatoohta <chisato.ohta.o1@gmail.com>
2026-06-19 22:11:21 +0900

    Move Supabase keys to env

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://owrgaldepieeqkpgwwsp.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cmdhbGRlcGllZXFrcGd3d3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MDQwNDEsImV4cCI6MjA5NzI4MDA0MX0.UfOcNOFbqcOA2Cn19r3xyeWb6RJgkUde2DhqtozjIY0'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
   supabaseUrl,
